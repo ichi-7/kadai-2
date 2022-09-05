68332179c4f417ec4fca6b16011a1709c59d1e0d
IkkanWatanabe <110508666+ichi-7@users.noreply.github.com>
2022-09-05 06:41:56 +0000

    [save2] kadai-2

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -9,7 +9,7 @@ class BooksController < ApplicationController
     @books = Book.all
     @book = Book.new(book_params)
     if @book.save
-      redirect_to "/books/#{@book.id}"
+      redirect_to "/books/#{@book.id}", notice: 'Book was successfully created.'
     else
       render :index
     end
@@ -29,7 +29,7 @@ class BooksController < ApplicationController
     book.update(book_params)
     
     if @book.update(book_params)
-      redirect_to "/books/#{@book.id}"
+      redirect_to "/books/#{@book.id}", notice: 'Book was successfully updated.'
     else
       render :edit
     end
